3e23af03f5b71c3149754f520e0e43d2985bf765
Jonas <jonaso@web.de>
2016-11-27 13:25:10 +0100

    fix sidenav

--- app/main.js
+++ app/main.js
@@ -40,11 +40,6 @@ $(function() {
     $('#legend_cleanliness').html(tpl);
 });
 
-// function closeDetails(e) {
-//     // e.preventDefault();
-//     $('#details').hide('fast');
-// }
-
 
 // Initialize the details side-nav
 $(function() {
@@ -53,6 +48,22 @@ $(function() {
       edge: 'right',
       closeOnClick: true,
       draggable: false // for touch screens
+    });
+    let $slideout = $('#slide-out');
+    $("#slide-out-btn").click(function () {
+        console.log('sidenav-open', $slideout.data('sidenav-open'));
+        if (!$slideout.data('sidenav-open')) {
+            // the sidenav is opening
+            $slideout.data('sidenav-open', true);
+            // store state
+        } else {
+            // the sidenav is closing
+            $slideout.data('sidenav-open', false);
+            // reset the car filter
+            carIdDim.filterAll();
+            // redraw with filtered car data
+            layers.cars = draw(carIdDim);
+        }
     });
 });
 
@@ -101,7 +112,9 @@ function bindClick(e) {
 
     // show the side-nav
     $('#slide-out-btn').sideNav('show');
+    $('#slide-out').data('sidenav-open', true);
 }
+
 
 
 // heat map (experimental)
@@ -216,7 +229,7 @@ fetch('/cars')
         // groups
         cleanlinessGroup = cleanlinessDim.group();
         locationGroup = locationDim.group();
-        timestampGroup = timestampDim.group();
+        timestampGroup = timestampDim.group(d3.timeMinute);
         carIdGroup = carIdDim.group();
         allGroup = allDim.groupAll();
 
@@ -247,7 +260,7 @@ fetch('/cars')
 
 
         layers.cars = draw(timestampDim);
-        console.log('layer', layers.cars);
+        // console.log('layer', layers.cars);
 
 
 
@@ -292,7 +305,258 @@ fetch('/cars')
         });
 
 
+
+
+
+        // init the date chart
+        $(function() {
+
+            // formatters
+            // var formatNumber = d3.format(",d"),
+            //     formatChange = d3.format("+,d"),
+            //     formatDate = d3.time.format("%B %d, %Y"),
+            //     formatTime = d3.time.format("%I:%M %p");
+
+            // A nest operator, for grouping the cars
+            var nestByDate = d3.nest()
+                .key(function(d) { return d3.time.day(d.date); });
+
+            let dateChart = barChart()
+                .dimension(timestampDim)
+                .group(timestampGroup)        //.round(d3.time.day.round)
+              .x(d3.time.scale()
+                .domain([new Date(2001, 0, 1), new Date(2001, 3, 1)]))
+                //.rangeRound([0, 10 * 90]))
+                //.filter([new Date(2001, 1, 1), new Date(2001, 2, 1)]);
+
+            let charts = [dateChart];
+
+            let chart = d3.selectAll(".chart")
+                .data(charts)
+                // .each(function(chart) { chart.on("brush", renderAll).on("brushend", renderAll); });
+
+
+            // Whenever the brush moves, re-rendering everything.
+            function renderChart() {
+                console.log('renderCHart');
+                chart.each(render);
+            }
+
+            renderChart();
+
+            window.filter = function(filters) {
+                filters.forEach(function(d, i) {
+                    charts[i].filter(d);
+                });
+                renderChart();
+            };
+
+            window.reset = function(i) {
+                charts[i].filter(null);
+                renderChart();
+            };
+
+        });
+
+
 	// }).catch(function(ex) {
 	// 	console.error('Error:', ex)
 	});
 
+
+
+  // see view-source:http://square.github.io/crossfilter/
+  function barChart() {
+    if (!barChart.id) {
+        barChart.id = 0;
+    }
+
+    var margin = {top: 10, right: 10, bottom: 20, left: 10},
+        x,
+        y = d3.scaleLinear().range([100, 0]),
+        id = barChart.id++,
+        axis = d3.axisBottom(),
+        // brush = d3.svg.brush(),
+        brushDirty,
+        dimension,
+        group,
+        round;
+
+    function chart(div) {
+      var width = x.range()[1],
+          height = y.range()[0];
+
+      y.domain([0, group.top(1)[0].value]);
+
+      div.each(function() {
+        var div = d3.select(this),
+            g = div.select("g");
+
+        // Create the skeletal chart.
+        if (g.empty()) {
+          div.select(".title").append("a")
+              .attr("href", "javascript:reset(" + id + ")")
+              .attr("class", "reset")
+              .text("reset")
+              .style("display", "none");
+
+          g = div.append("svg")
+              .attr("width", width + margin.left + margin.right)
+              .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+              .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+          g.append("clipPath")
+              .attr("id", "clip-" + id)
+            .append("rect")
+              .attr("width", width)
+              .attr("height", height);
+
+          g.selectAll(".bar")
+              .data(["background", "foreground"])
+            .enter().append("path")
+              .attr("class", function(d) { return d + " bar"; })
+              .datum(group.all());
+
+          g.selectAll(".foreground.bar")
+              .attr("clip-path", "url(#clip-" + id + ")");
+
+          g.append("g")
+              .attr("class", "axis")
+              .attr("transform", "translate(0," + height + ")")
+              .call(axis);
+
+          // Initialize the brush component with pretty resize handles.
+          // var gBrush = g.append("g").attr("class", "brush").call(brush);
+          // gBrush.selectAll("rect").attr("height", height);
+          // gBrush.selectAll(".resize").append("path").attr("d", resizePath);
+        }
+
+        // Only redraw the brush if set externally.
+        // if (brushDirty) {
+        //   brushDirty = false;
+        //   g.selectAll(".brush").call(brush);
+        //   div.select(".title a").style("display", brush.empty() ? "none" : null);
+        //   if (brush.empty()) {
+        //     g.selectAll("#clip-" + id + " rect")
+        //         .attr("x", 0)
+        //         .attr("width", width);
+        //   } else {
+        //     var extent = brush.extent();
+        //     g.selectAll("#clip-" + id + " rect")
+        //         .attr("x", x(extent[0]))
+        //         .attr("width", x(extent[1]) - x(extent[0]));
+        //   }
+        // }
+
+        g.selectAll(".bar").attr("d", barPath);
+      });
+
+      function barPath(groups) {
+        var path = [],
+            i = -1,
+            n = groups.length,
+            d;
+        while (++i < n) {
+          d = groups[i];
+          path.push("M", x(d.key), ",", height, "V", y(d.value), "h9V", height);
+        }
+        return path.join("");
+      }
+
+      function resizePath(d) {
+        var e = +(d == "e"),
+            x = e ? 1 : -1,
+            y = height / 3;
+        return "M" + (.5 * x) + "," + y
+            + "A6,6 0 0 " + e + " " + (6.5 * x) + "," + (y + 6)
+            + "V" + (2 * y - 6)
+            + "A6,6 0 0 " + e + " " + (.5 * x) + "," + (2 * y)
+            + "Z"
+            + "M" + (2.5 * x) + "," + (y + 8)
+            + "V" + (2 * y - 8)
+            + "M" + (4.5 * x) + "," + (y + 8)
+            + "V" + (2 * y - 8);
+      }
+    }
+
+    // brush.on("brushstart.chart", function() {
+    //   var div = d3.select(this.parentNode.parentNode.parentNode);
+    //   div.select(".title a").style("display", null);
+    // });
+
+    // brush.on("brush.chart", function() {
+    //   var g = d3.select(this.parentNode),
+    //       extent = brush.extent();
+    //   if (round) g.select(".brush")
+    //       .call(brush.extent(extent = extent.map(round)))
+    //     .selectAll(".resize")
+    //       .style("display", null);
+    //   g.select("#clip-" + id + " rect")
+    //       .attr("x", x(extent[0]))
+    //       .attr("width", x(extent[1]) - x(extent[0]));
+    //   dimension.filterRange(extent);
+    // });
+
+    // brush.on("brushend.chart", function() {
+    //   if (brush.empty()) {
+    //     var div = d3.select(this.parentNode.parentNode.parentNode);
+    //     div.select(".title a").style("display", "none");
+    //     div.select("#clip-" + id + " rect").attr("x", null).attr("width", "100%");
+    //     dimension.filterAll();
+    //   }
+    // });
+
+    chart.margin = function(_) {
+      if (!arguments.length) return margin;
+      margin = _;
+      return chart;
+    };
+
+    chart.x = function(_) {
+      if (!arguments.length) return x;
+      x = _;
+      axis.scale(x);
+      // brush.x(x);
+      return chart;
+    };
+
+    chart.y = function(_) {
+      if (!arguments.length) return y;
+      y = _;
+      return chart;
+    };
+
+    chart.dimension = function(_) {
+      if (!arguments.length) return dimension;
+      dimension = _;
+      return chart;
+    };
+
+    chart.filter = function(_) {
+      if (_) {
+        // brush.extent(_);
+        dimension.filterRange(_);
+      } else {
+        // brush.clear();
+        dimension.filterAll();
+      }
+      // brushDirty = true;
+      return chart;
+    };
+
+    chart.group = function(_) {
+      if (!arguments.length) return group;
+      group = _;
+      return chart;
+    };
+
+    chart.round = function(_) {
+      if (!arguments.length) return round;
+      round = _;
+      return chart;
+    };
+
+    // return d3.rebind(chart, brush, "on");
+    return d3.rebind(chart, null, "on");
+  }
